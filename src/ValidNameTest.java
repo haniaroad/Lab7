839ada34f4231755431e9b3f42c9f2aeb6e972c1
Derien <haniaroadlifestyle@gmail.comgit config --listgit config --listgit config --global user.name = Deriengit config --global user.email = haniaroadlifestyle@gmail.com>
2018-10-16 08:57:21 -0400

    checkName push

---
+++ src/ValidNameTest.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class ValidNameTest {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner (System.in);
+		
+		Validator v = new Validator();
+		
+		System.out.println(Validator.isValidName(scan));
+		
+		
+
+	}
+
+}
